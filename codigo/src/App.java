cb2897d10a89397b774f4b6613e777641a1947de
João Vitor Bessa Lacerda <80555195+jvjzn@users.noreply.github.com>
2023-04-04 10:21:57 -0300

    Update App.java

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -5,62 +5,41 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         //Expecificando o nome do arquivo para leitura
+    	
         System.out.print("Insira o nome do arquivo>> ");
         String nomeArquivo = MyIO.readString();
-
+        
+        
+        //Lendo Arquivo
+        //Arquivo deve conter um caractere declarando o tipo de grafo
+        //"C" Completo,"D" Direcionado,"N" Não Direcionado
+        //Ler primeira linha Char e atribuir a "tipoGrafo"
+        //Padrão ler "xx;xx;xx"
+        //Realizar split e alocar 
+        /*ArquivoTextoLeitura file = new ArquivoTextoLeitura(nomeArquivo);*/
+        Char tipoGrafo;
+        
         //Definindo o nome do grafo
         System.out.print("Insira o nome do Grafo>> ");
-        String nomeGrafo = MyIO.readString();
-        
-        int op1;
-
-        do {
-            System.out.println("Digite uma das opções abaixo: ");
-            System.out.println("[1] Grafo Completo");
-            System.out.println("[2] Grafo Mutável");
-            System.out.println("[0] Finalizar programa");
-            System.out.print(">> ");
-            op1 = MyIO.readInt();
-            switch (op1){
-                case 1:
-                    //Inicializado e atribuindo nome ao grafo
-                    GrafoCompleto novoGrafoC = new GrafoCompleto(nomeGrafo); 
+        String nomeGrafo = MyIO.readString();       
+            
+            switch (tipoGrafo){
+                case C:
+                     
                     break;
-                case 2:
-                    int op2;
-                    
-                    do{
-                        System.out.println("Digite uma das opções abaixo: ");
-                        System.out.println("[1] Direcionado");
-                        System.out.println("[2] Não Direcionado");
-                        System.out.println("[0] Finalizar programa");
-                        System.out.print(">> ");
-                        op2 = MyIO.readInt();
-
-                        switch (op2){
-                            case 1:
-                                //Inicializado e atribuindo nome ao grafo
-                                GrafoDirecionado novoGrafoD = new GrafoDirecionado(nomeGrafo);
-                                
-                                break;
-                            case 2:
-                                //Inicializado e atribuindo nome ao grafo
-                                GrafoNaoDirecionado novoGrafoND = new GrafoNaoDirecionado(nomeGrafo);
-                                
-                                break;
-                        }
-                    }while(op2!=0);
-                        
+                case D:
                     
                     break;
-                case 0:
+                case N:
+                	
+                	break;
+                case F:
                     System.out.println("Finalizando programa.");
                     break;
                 default:
                     System.out.println("Digite uma opção válida.");
             }
-
-        }while (1!=0);
-
+            
+            System.out.println("");
     }
 }
