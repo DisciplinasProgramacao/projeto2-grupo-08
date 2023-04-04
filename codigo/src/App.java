852747934295599e0e2f87763b2441d067125757
João Vitor Bessa Lacerda <80555195+jvjzn@users.noreply.github.com>
2023-04-04 19:44:37 -0300

    Update App.java

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -1,15 +1,15 @@
 import app.*;
+
 public class App {
 
-      
+
     public static void main(String[] args) throws Exception {
 
         //Expecificando o nome do arquivo para leitura
-    	
         System.out.print("Insira o nome do arquivo>> ");
         String nomeArquivo = MyIO.readString();
-        
-        
+
+
         //Lendo Arquivo
         //Arquivo deve conter um caractere declarando o tipo de grafo
         //"D" Direcionado,"N" Não Direcionado
@@ -17,27 +17,48 @@ public class App {
         //Padrão ler "xx;xx;xx"
         //Realizar split e alocar 
         /*ArquivoTextoLeitura file = new ArquivoTextoLeitura(nomeArquivo);*/
-        Char tipoGrafo;
-        
+
         //Definindo o nome do grafo
+        char tipoGrafo = ' ';
         System.out.print("Insira o nome do Grafo>> ");
-        String nomeGrafo = MyIO.readString();       
-            
-            switch (tipoGrafo){
-                case D:
+        String nomeGrafo = MyIO.readString();
+
+        switch (tipoGrafo) {
+            case 'D':
+                GrafoDirecionado novoGrafoDirecionado = new GrafoDirecionado(nomeGrafo);
+
+                break;
+            case 'N':
+                GrafoNaoDirecionado novoGrafoNaoDirecionado = new GrafoNaoDirecionado(nomeGrafo);
+
+                break;
+            case 'F':
+                System.out.println("Finalizando programa.");
+                break;
+            default:
+                System.out.println("Digite uma opção válida.");
+        }
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Imprimir subgrafo?");
+        System.out.println("[1]Sim");
+        System.out.println("[2]Finalizar programa");
+        System.out.print(">> ");
+
+        int op = MyIO.readInt();
+
+        do {
+            switch (op) {
+                case 1:
                     
                     break;
-                case N:
-                	
-                	break;
-                case F:
+                case 2:
                     System.out.println("Finalizando programa.");
                     break;
                 default:
                     System.out.println("Digite uma opção válida.");
             }
-            
-            System.out.println("");
+        } while (op != 0);
     }
 }
 
