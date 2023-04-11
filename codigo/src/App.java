cfb8f03f71cf9cf342a427e87fca28b8c7640ac1
João Vitor Bessa Lacerda <80555195+jvjzn@users.noreply.github.com>
2023-04-11 00:13:01 -0300

    Update App.java

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -1,64 +1,107 @@
-// import app.*;
+ import app.*;
 
-// public class App {
+ public class App {
 
-
-//     public static void main(String[] args) throws Exception {
-
-//         //Expecificando o nome do arquivo para leitura
-//         System.out.print("Insira o nome do arquivo>> ");
-//         String nomeArquivo = MyIO.readString();
-
-
-//         //Lendo Arquivo
-//         //Arquivo deve conter um caractere declarando o tipo de grafo
-//         //"D" Direcionado,"N" Não Direcionado
-//         //Ler primeira linha Char e atribuir a "tipoGrafo"
-//         //Padrão ler "xx;xx;xx"
-//         //Realizar split e alocar 
-//         /*ArquivoTextoLeitura file = new ArquivoTextoLeitura(nomeArquivo);*/
-
-//         //Definindo o nome do grafo
-//         char tipoGrafo = ' ';
-//         System.out.print("Insira o nome do Grafo>> ");
-//         String nomeGrafo = MyIO.readString();
-
-//         switch (tipoGrafo) {
-//             case 'D':
-//                 GrafoDirecionado novoGrafoDirecionado = new GrafoDirecionado(nomeGrafo);
-
-//                 break;
-//             case 'N':
-//                 GrafoNaoDirecionado novoGrafoNaoDirecionado = new GrafoNaoDirecionado(nomeGrafo);
-
-//                 break;
-//             case 'F':
-//                 System.out.println("Finalizando programa.");
-//                 break;
-//             default:
-//                 System.out.println("Digite uma opção válida.");
-//         }
-
-//         System.out.println("=-=-=-=-=-=-=-=-=-=-=");
-//         System.out.println("Imprimir subgrafo?");
-//         System.out.println("[1]Sim");
-//         System.out.println("[2]Finalizar programa");
-//         System.out.print(">> ");
-
-//         int op = MyIO.readInt();
-
-//         do {
-//             switch (op) {
-//                 case 1:
-                    
-//                     break;
-//                 case 2:
-//                     System.out.println("Finalizando programa.");
-//                     break;
-//                 default:
-//                     System.out.println("Digite uma opção válida.");
-//             }
-//         } while (op != 0);
+//     public static void metodoLeitura(){
+//
 //     }
-// }
 
+     public static void main(String[] args) throws Exception {
+
+         //Expecificando o nome do arquivo para leitura
+         System.out.print("Insira o nome do arquivo>> ");
+         String nomeArquivo = MyIO.readString();
+
+
+         //Lendo Arquivo
+         ArquivoTextoLeitura file = new ArquivoTextoLeitura(nomeArquivo);
+
+         String tipoGrafo = file.lerArquivo();
+
+         String entrada;
+
+         //expecificações arquivo
+//        - primeira linha deve conter char que declara o tipo do grafo
+//        - segunda linha pra frente, tipo de leitura "x;y;z"
+//        - x → origem / y → destino / z → peso
+//        - é realizado um Split a cada ";" e valores são atribuídos a variáveis "origem", "desino", "peso"
+//        - caso origem e destino existam, criar aresta
+//        - caso não existam, criar vértices com o respectivo id e criar aresta
+//        - ao final um flag "FIM" interrompendo leitura
+
+         //Definindo o nome do grafo
+
+
+         System.out.print("Insira o nome do Grafo>> ");
+         String nomeGrafo = MyIO.readString();
+
+         if(tipoGrafo == "D") {
+             GrafoDirecionado novoGrafo = new GrafoDirecionado(nomeGrafo);
+             while (file.lerArquivo() != "FIM") {
+                 entrada = MyIO.readLine();
+
+                 int origem = Integer.parseInt(entrada.split(";")[0]);
+                 if (novoGrafo.existeVertice(origem) == null) {
+                     novoGrafo.addVertice(origem);
+                 }
+                 int destino = Integer.parseInt(entrada.split(";")[1]);
+                 if (novoGrafo.existeVertice(destino) == null) {
+                     novoGrafo.addVertice(origem);
+                 }
+                 int peso = Integer.parseInt(entrada.split(";")[2]);
+
+                 novoGrafo.addAresta(origem, destino, peso);
+             }
+         } else if (tipoGrafo == "N"){
+             GrafoNaoDirecionado novoGrafo = new GrafoNaoDirecionado(nomeGrafo);
+             while(file.lerArquivo() != "FIM"){
+                 entrada = MyIO.readLine();
+
+                 int origem = Integer.parseInt(entrada.split(";")[0]);
+                 if (novoGrafo.existeVertice(origem) == null){
+                     novoGrafo.addVertice(origem);
+                 }
+                 int destino = Integer.parseInt(entrada.split(";")[1]);
+                 if (novoGrafo.existeVertice(destino) == null){
+                     novoGrafo.addVertice(origem);
+                 }
+                 int peso = Integer.parseInt(entrada.split(";")[2]);
+
+                 novoGrafo.addAresta(origem,destino,peso);
+             }
+         }
+
+         System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+         System.out.println("[1]Imprimir subgrafo");
+         System.out.println("[2]Busca em profundidade");
+         System.out.println("[0]Finalizar programa");
+         System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+         System.out.print(">> ");
+
+         int op = MyIO.readInt();
+
+         do {
+             switch (op) {
+                 case 1:
+                        if (tipoGrafo == "D"){
+                            //novoGrafo.subgrafo();
+                        }
+                        else {
+                            //novoGrafo.buscaEmProfundidade();
+                        }
+                     break;
+                 case 2:
+
+                     break;
+                 case 0:
+                     System.out.println("Finalizando programa.");
+                     break;
+                 default:
+                     System.out.println("Digite uma opção válida.");
+             }
+         } while (op != 0);
+
+         //Escrever grafo em arquivo .txt
+
+     }
+ }
